5998bb87596dbb976d65ede76e6143db59d738df
Adam Evans <adam3van5@gmail.com>
2024-07-09 21:49:12 +0900

    useRef example usage

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,21 @@
+import { useState, useRef } from 'react';
+
 export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+  const playerNameRef = useRef();
+
+  function handleClick() {
+    // useRef will always return an object with a .current property
+    setEnteredPlayerName(playerNameRef.current.value);
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>
+        Welcome {enteredPlayerName ? enteredPlayerName : 'unknown entity'}
+      </h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerNameRef} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
